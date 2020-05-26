a5f72424d8b26d1af39d632c2dc5b1b3886bb0a7
Ankita Shah Ashwin <ankita.shah.ashwin@ipk.fraunhofer.de>
2020-05-26 14:43:08 +0200

    home page container (component)

---
+++ my-app/src/components/HomePage/HomePage.js
@@ -0,0 +1,54 @@
+import React from "react";
+import "antd/dist/antd.css";
+import "../../styles.css";
+import { PageHeader, Carousel } from "antd";
+
+export default class HomePage extends React.Component {
+  render() {
+    return (
+      <div>
+        <PageHeader
+          className="site-page-header"
+          title="Welcome to Rick and Morty show"
+          data-test="homepage-header"
+        />
+        <Carousel autoplay dots={true} key="carousel">
+          <img
+            src="/images/homeimage.jpg"
+            alt="Girl in a jacket"
+            width="100%"
+            height="600"
+          />
+
+          <img
+            src="/images/homeimage2.jpg"
+            alt="Girl in a jacket"
+            width="100%"
+            height="600"
+          />
+
+          <img
+            src="/images/homeimage3.jpg"
+            alt="Girl in a jacket"
+            width="100%"
+            height="600"
+          />
+
+          <img
+            src="/images/homeimage4.jpg"
+            alt="Girl in a jacket"
+            width="100%"
+            height="600"
+          />
+
+          <img
+            src="/images/homeimage5.jpg"
+            alt="Girl in a jacket"
+            width="100%"
+            height="600"
+          />
+        </Carousel>
+      </div>
+    );
+  }
+}
